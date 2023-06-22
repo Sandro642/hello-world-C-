e3f39704d6552f0a3c6d71b145c341d2e31e3e3c
Sandro642 <sandro33810@gmail.com>
2023-06-22 19:26:57 +0200

    Initial commit

---
+++ hello-world/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main()
+    {
+        var H = "Hello World!";
+        var W = new Func<Func<Func<string>>>(() => (() => (() => H)));
+
+        var HFunc = W()()();
+
+        Console.WriteLine(HFunc);
+
+        Console.ReadKey();
+    }
+}
